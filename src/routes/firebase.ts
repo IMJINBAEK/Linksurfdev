7f65dfb183ceb37785a2d77e6795db9d523cfdd5
IMJINBAEK <40750621+IMJINBAEK@users.noreply.github.com>
2024-03-01 10:54:11 +0000

    Initial Commit

---
+++ src/routes/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBWjLQj69MJFFY8XYUqB5eysxY1nWeLvAE",
+  authDomain: "linksurf-72722.firebaseapp.com",
+  projectId: "linksurf-72722",
+  storageBucket: "linksurf-72722.appspot.com",
+  messagingSenderId: "900205340535",
+  appId: "1:900205340535:web:8a21de2b4885474039e372",
+  measurementId: "G-LRC30N3Y9E"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app);
